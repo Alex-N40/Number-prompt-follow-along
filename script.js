8bb81c9c7e44af90cbbab0bee1bb9a0554df9dc4
Alex-N40 <alex_n40@proton.me>
2024-04-15 16:50:36 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var applesBananas = 5+8;
+console.log(applesBananas);
+//In console: 13
+
+console.log(100/2);
+//In console: 50
+
+var mealBudget = 30.36;
+var mealCost = 10.25;
+console.log(mealBudget - mealCost);
+console.log(`You have $${mealBudget - mealCost} remaining.`);
+//In console: 20.11
+//You have $20.11 remaining.
+
+var tvShows = Number("23");
+var movies = 12;
+console.log(tvShows + movies);
+//In console: 35
+
+var favoriteGenre = prompt("What's your favorite music genre?");
+console.log(favoriteGenre);
+//A pop up will prompt a user to enter their favorite genre.
+//In console if a user enters "Rock": Rock
+
+var oldFunds = 1500;
+var newFunds = Number(prompt("How much funds were raised?"));
+console.log(`The fundraiser total is now $${oldFunds + newFunds}!`);
+//A pop up will prompt a user to enter a number for funds raised.
+//In console if a user enters "50": 1550
+
+
+
+
+
